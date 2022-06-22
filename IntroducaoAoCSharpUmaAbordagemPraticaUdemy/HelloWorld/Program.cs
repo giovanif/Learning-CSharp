87658e4c373ec2798216c93f83b6d85e19a8c891
Giovani Faleiros <agokdroid@gmail.com>
2022-06-22 17:56:18 -0300

    Introdução ao CSharp Modulo 1 - Aulas 1 a 4

---
+++ IntroducaoAoCSharpUmaAbordagemPraticaUdemy/HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Pasta lógica
+namespace HelloWorld
+{
+    internal class Program
+    {
+        //Iso é um comentário de uma linha! Tudo que for escrito será ignorado pelo compilador
+
+        /* Este
+         * é
+         * um
+         * comentário
+         * de
+         * mais
+         * de
+         * uma
+         * linha
+         */
+
+        // Método Main:
+        // Ponto de entrada para a aplicação, ou seja, é
+        // o que define o início da aplicação
+        static void Main(string[] args)
+        {
+            // Imprimir na tela o famoso "Olá, Mundo!"
+            Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("Meu nome é Giovani!");
+        }
+    }
+}
---
+++ IntroducaoAoCSharpUmaAbordagemPraticaUdemy/TiposEVariaveis/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposEVariaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 10; // Declara uma variável inteira e armazena o valor 10 nela
+            int numero2 = 20;
+            var soma = numero1 + numero2; // O var define dinamicamente o tipo da variável
+            Console.WriteLine("10 + 20 = " + soma);
+
+            int copiaDeNumero1 = numero1; // Copia o valor da variável 'numero1'
+            copiaDeNumero1 = 11; // Será que o valor de 'numero1' foi alterado?
+            Console.WriteLine(numero1);
+            Console.WriteLine(copiaDeNumero1);
+
+            var quadrado1 = new Quadrado(); // Cria um quadrado
+            quadrado1.lado = 10; // quadrado1 terá valor de lado = 10
+            var quadrado2 = quadrado1; // Será que a  cópia do quadrado foi completa ou copiamos apenas a referência?
+            //quadrado2.lado = 11;
+            Console.WriteLine(quadrado1.lado); // quadrado1 deveria ter o lado igual a 10
+            Console.WriteLine(quadrado2.lado); // quadrado2 deveria ter o lado igual a 11
+        }
+
+        class Quadrado // Define uma classe chamada Quadrado
+        {
+            public int lado; // Define um atributo inteiro chamado lado na classe Quadrado
+        }
+    }
+}
